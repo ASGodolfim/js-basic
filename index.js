01f6ebe6b374a443e48d6843c7d204b801bfbd0d
ASGodolfim <stonegodolfim@gmail.com>
2024-10-10 11:23:32 -0300

    feat read write files

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hello World')
+
+let firstName = 'Mosh';
+let lastName = 'Godoi';
+let age = 30;
+let selectColor = null;
+let isApproved = true;
+typeof firstName;
+typeof age;
+typeof selectColor;
+typeof isApproved;
+
+let person = {
+    firstName: 'Mosh',
+    lastName: 'Godoi',
+    age: 29,
+    isApproved: true
+}
+
+function greet (name){
+    console.log('Hello' + name)
+}
+
+greet(person.firstName)
