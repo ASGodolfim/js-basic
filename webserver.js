e584466134beddee0e4e956eac7dae4f30039d3a
ASGodolfim <stonegodolfim@gmail.com>
2024-10-10 15:06:19 -0300

    feat overview finished

--- webserver.js
+++ webserver.js
@@ -3,23 +3,55 @@ const url = require('url');
 const fs = require('fs');
 
 
-fs.readFileSync(`./dev-data/data.json`, 'utf-8', (err, data) =>{
-    const productData = JSON.parse(data);
-});
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%NAME%}/g, product.productName);
+    output = output.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`./template/overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`./template/card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`./template/product.html`, 'utf-8');
+
+const data = fs.readFileSync(`./dev-data/data.json`, 'utf-8');
+const productData = JSON.parse(data);
+
 
 const server = http.createServer((req, res) =>{
 
     const pathName = req.url;
 
     if(pathName === '/' || pathName === '/overview') {
-        res.end('This is the Overview!');
-    } else if (pathName === '/product') {
-        res.end('This is the Product!');
-    } else if (pathName === '/api'){
-        console.log(productData);
+        res.writeHead(200, {"content-type": "text/html"});
+
+        const cardsHtml = productData.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARD%}', cardsHtml);
+
+        res.end(output);
+    }
+    
+    
+    else if (pathName === '/product') {
+        res.writeHead(200, {"content-type": "text/html"});
+        res.end(tempProduct);
+    }
+    
+    
+    else if (pathName === '/api'){
         res.writeHead(200, {"content-type": "application/json"});
         res.end(data);
-    } else {
+    }
+    
+    
+    else {
         res.writeHead(404, {
             'Content-type': 'text/html',
             'my-own-header': 'hello-world'
